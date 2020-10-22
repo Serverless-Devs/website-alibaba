652e0386e1b07148338a56088766c92bf0e73337
XianB <xianbintang@outlook.com>
2020-10-22 11:40:30 +0800

    update prefix and env

--- src/command-impl/deploy.js
+++ src/command-impl/deploy.js
@@ -4,30 +4,23 @@ const path = require('path')
 const { green, yellow, blue, red} = require('colors')
 const OssClient = require('../services/oss')
 
-// const {
-//   AddCdnDomain, DescribeUserDomains, UpdateTagResources, DescribeCdnDomainConfigs, SetCdnDomainConfig,
-//   DeleteSpecificConfig, SetDomainServerCertificate, DescribeCdnCertificateList,
-// } = require('../oss/cdn')
-
-// TODO check param for each config
 const deploy = async (params) => {
   const oss = new OssClient(params.credentials, params.region, params.bucket)
   let path = params.env.Path
   let content = params.env.Content
   let codeUri = params.codeUri
-  // console.log(path)
-  // console.log(content)
   let jsonContent = toJsonContent(content)
-  let targetObject = codeUri.ObjectPrefix + "/" + path
+  let prefix = ""
+  if (codeUri.ObjectPrefix) {
+    prefix = codeUri.ObjectPrefix
+  }
+  let targetObject = prefix + "/" + path
   await oss.putBuffer(targetObject, jsonContent)
-  // await oss.uploadFiles(objectPrefix, localDir, includes, excludes)
 }
 
 function toJsonContent(content) {
   let json = {}
   for (const kv of content) {
-    // console.log(kv.Key)
-    // console.log(kv.Value)
     json[kv.Key] = kv.Value
   }
   return `var env = ` + JSON.stringify(json)
