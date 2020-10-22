f163da40ab77ca7f0e2237c55d6e2fa3805c7168
XianB <xianbintang@outlook.com>
2020-10-22 14:26:26 +0800

    update

--- src/command-impl/remove.js
+++ src/command-impl/remove.js
@@ -2,17 +2,10 @@
 
 const path = require('path')
 const { green, yellow, blue, red} = require('colors')
-// const {
-//   AddCdnDomain, DescribeUserDomains, UpdateTagResources, DescribeCdnDomainConfigs, SetCdnDomainConfig,
-//   DeleteSpecificConfig, SetDomainServerCertificate, DescribeCdnCertificateList,
-// } = require('../oss/cdn')
 
-// TODO check param for each config
 const remove = async (inputParams) => {
   const {
-    credentials, state, args, cdnDomain, tags, ipv6,
-    others, force, accessControl, performance, video,
-    backToOrigin, cache, https, domainName,
+    credentials, state, args,
   } = inputParams
 }
 
